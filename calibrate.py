be548fd03b31ab4ccec6052787de1bd1c2d71bff
Mikkel Stårvik <mikkel.starvik@gmail.com>
2025-02-05 11:23:05 +0100

    Implement downloading and parsing catalogs

---
+++ calibrate.py
@@ -0,0 +1,6 @@
+from catalog import load_catalog
+
+
+if __name__ == "__main__":
+    catalog = load_catalog("SAO")
+    print(catalog)
---
+++ catalog.py
@@ -0,0 +1,193 @@
+from abc import ABC, abstractmethod
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from pathlib import Path
+from struct import Struct
+from tqdm import tqdm
+import numpy as np
+import requests
+
+
+class _Catalog(ABC):
+    uri: str
+    filename: str
+
+    @property
+    def cache_dir(self):
+        return Path(__file__).parent / ".cache" / "night_sky_camera_calibration"
+
+    @property
+    def path(self):
+        return self.cache_dir / self.filename
+
+    def download(self):
+        self.cache_dir.mkdir(parents=True, exist_ok=True)
+        with requests.get(self.uri, stream=True) as response:
+            response.raise_for_status()
+            filesize = int(response.headers.get("content-length", 0))
+            with open(self.path, "wb") as file, tqdm(
+                desc=self.filename,
+                total=filesize,
+                unit="B",
+                unit_scale=True,
+                unit_divisor=1024,
+            ) as bar:
+                for chunk in response.iter_content(chunk_size=1024):
+                    file.write(chunk)
+                    bar.update(len(chunk))
+
+    def load(self):
+        if not self.path.exists():
+            self.download()
+        return self.read()
+
+    @abstractmethod
+    def read(self):
+        pass
+
+
+class BSC5(_Catalog):
+    uri = "http://tdc-www.harvard.edu/catalogs/BSC5"
+    filename = "BSC5"
+
+    header_type = Struct("<7i")
+    entry_type = Struct("<f2d2ch2f")
+
+    def read(self):
+        with open(self.path, "rb") as file:
+            header = self.header_type.unpack(file.read(self.header_type.size))
+            epoch = "J2000" if header[2] < 0 else "B1950"
+            size = abs(header[2])
+            ra = np.zeros(size)
+            dec = np.zeros(size)
+            pm_ra = np.zeros(size)
+            pm_dec = np.zeros(size)
+            vmag = np.zeros(size)
+            for i in range(size):
+                entry = self.entry_type.unpack(file.read(self.entry_type.size))
+                ra[i] = entry[1]
+                dec[i] = entry[2]
+                pm_ra[i] = entry[6]
+                pm_dec[i] = entry[7]
+                vmag[i] = entry[5] / 100
+        return (
+            SkyCoord(
+                ra=ra * u.rad,
+                dec=dec * u.rad,
+                pm_ra_cosdec=pm_ra * np.cos(dec) * u.rad/u.yr,
+                pm_dec=pm_dec * u.rad/u.yr,
+                obstime=epoch
+            ),
+            vmag
+        )
+
+
+class PPM(_Catalog):
+    uri = "http://tdc-www.harvard.edu/catalogs/PPM"
+    filename = "PPM"
+
+    header_type = Struct(">7i")
+    entry_type = Struct(">2d2ch2f")
+
+    def read(self):
+        with open(self.path, "rb") as file:
+            header = self.header_type.unpack(file.read(self.header_type.size))
+            size = header[2]
+            ra = np.zeros(size)
+            dec = np.zeros(size)
+            pm_ra = np.zeros(size)
+            pm_dec = np.zeros(size)
+            vmag = np.zeros(size)
+            for i in range(size):
+                entry = self.entry_type.unpack(file.read(self.entry_type.size))
+                ra[i] = entry[0]
+                dec[i] = entry[1]
+                pm_ra[i] = entry[5]
+                pm_dec[i] = entry[6]
+                vmag[i] = entry[4] / 100
+        return (
+            SkyCoord(
+                ra=ra * u.rad,
+                dec=dec * u.rad,
+                pm_ra_cosdec=pm_ra * np.cos(dec) * u.rad/u.yr,
+                pm_dec=pm_dec * u.rad/u.yr,
+                obstime="B1950"
+            ),
+            vmag
+        )
+
+
+class IRAS(_Catalog):
+    uri = "http://tdc-www.harvard.edu/catalogs/IRAS"
+    filename = "IRAS"
+
+    header_type = Struct(">7i")
+    entry_type = Struct(">f2d2c4h")
+
+    def read(self):
+        with open(self.path, "rb") as file:
+            header = self.header_type.unpack(file.read(self.header_type.size))
+            size = header[2]
+            ra = np.zeros(size)
+            dec = np.zeros(size)
+            for i in range(size):
+                entry = self.entry_type.unpack(file.read(self.entry_type.size))
+                ra[i] = entry[1]
+                dec[i] = entry[2]
+        return (
+            SkyCoord(
+                ra=ra * u.rad,
+                dec=dec * u.rad,
+                obstime="B1950"
+            ),
+            None
+        )
+
+
+class SAO(_Catalog):
+    uri = "http://tdc-www.harvard.edu/catalogs/SAO"
+    filename = "SAO"
+
+    header_type = Struct(">7i")
+    entry_type = Struct(">2d2ch2f")
+
+    def read(self):
+        with open(self.path, "rb") as file:
+            header = self.header_type.unpack(file.read(self.header_type.size))
+            epoch = "J2000" if header[2] < 0 else "B1950"
+            size = abs(header[2])
+            ra = np.zeros(size)
+            dec = np.zeros(size)
+            pm_ra = np.zeros(size)
+            pm_dec = np.zeros(size)
+            vmag = np.zeros(size)
+            for i in range(size):
+                entry = self.entry_type.unpack(file.read(self.entry_type.size))
+                ra[i] = entry[0]
+                dec[i] = entry[1]
+                pm_ra[i] = entry[5]
+                pm_dec[i] = entry[6]
+                vmag[i] = entry[4] / 100
+        return (
+            SkyCoord(
+                ra=ra * u.rad,
+                dec=dec * u.rad,
+                pm_ra_cosdec=pm_ra * np.cos(dec) * u.rad/u.yr,
+                pm_dec=pm_dec * u.rad/u.yr,
+                obstime=epoch
+            ),
+            vmag
+        )
+
+
+catalogs = {
+    "BSC5": BSC5,
+    "IRAS": IRAS,
+    "PPM": PPM,
+    "SAO": SAO
+}
+
+
+def load_catalog(name: str):
+    catalog = catalogs[name]()
+    return catalog.load()
